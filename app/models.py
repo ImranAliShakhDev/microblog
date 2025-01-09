0fb5c5c2f65149137ca34232049fb23d63022441
Imran Ali-Shakh <imranalishah829@gmail.com>
2025-01-09 12:07:07 +0200

    Refacturing of application structure

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timezone
-from time import time
 from hashlib import md5
-import jwt
+from time import time
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import app, db, login
+import jwt
+from app import db, login
 
 
 followers = sa.Table(
@@ -91,18 +92,18 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
-    
+
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256')
-    
+            current_app.config['SECRET_KEY'], algorithm='HS256')
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
-        except:
+        except Exception:
             return
         return db.session.get(User, id)
 
@@ -119,10 +120,9 @@ class Post(db.Model):
         index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
                                                index=True)
+    language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))
 
     author: so.Mapped[User] = so.relationship(back_populates='posts')
-
-    language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
